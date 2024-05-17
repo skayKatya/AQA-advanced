c218961d010498a910ddf0bdadc5a703f573d200
skayKatya <82179251+skayKatya@users.noreply.github.com>
2024-05-17 14:36:15 +0300

    Update tasks3.js

--- jsBasics/tasks3.js
+++ jsBasics/tasks3.js
@@ -1,6 +1,4 @@
-let personAge1 = 15
-let personAge2 = 25
+let personAge = 19
 let personIsAdult = true
 
-console.log(personAge1 > 18, `- Person is'n an adult`)
-console.log(personAge2 > 18, `- Person is an adult`)
+console.log(personAge > 18? personIsAdult : !personIsAdult)
