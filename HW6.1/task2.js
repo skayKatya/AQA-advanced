04f55f2dafc99e42fc4e8c9ddab612fde5d184cd
Kate <skay.katya@gmail.com>
2024-05-27 19:05:52 +0300

    HW-6.1

---
+++ HW6.1/task2.js
@@ -0,0 +1,17 @@
+function isAdult (age) {
+    return age >=18 ? true: false
+}
+
+console.log(isAdult(15))
+console.log(isAdult(18))
+console.log(isAdult(25))
+
+//////////////
+
+// const isAdult = (age) => {
+//     return age >=18 ? true: false
+// }
+
+// console.log(isAdult(15))
+// console.log(isAdult(18))
+// console.log(isAdult(25))
---
+++ HW6.1/task3.js
@@ -0,0 +1,13 @@
+const checkOrder = (available, ordered) => {
+    let result
+    if (available < ordered){
+        result = 'Your order is too large, we don\’t have enough goods.';
+    } else if (ordered == 0){
+        result = 'Your order is empty';
+    } else if (available >= ordered) {
+        result = 'Your order is accepted';
+    }
+    return result;
+}
+
+console.log(checkOrder(45,0))
