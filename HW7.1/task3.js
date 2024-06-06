8da81d16448b2202b4d75dc75a48f50b2d516f1d
skayKatya <82179251+skayKatya@users.noreply.github.com>
2024-06-06 17:06:59 +0300

    Update task3.js

--- HW7.1/task3.js
+++ HW7.1/task3.js
@@ -12,25 +12,23 @@ function divide (numerator, denumerator) {
 
 
 // function divide (numerator, denumerator) {
-//    return numerator/denumerator
-//  }
-
-// try {
-//         if (!(typeof numerator === 'number') || !(typeof denumerator === 'number')) {
-//             throw new Error('Parameters are not a number')
-//         } else if (denumerator === 0) {
-//             throw new Error('Dividing to 0 is forbitten')
-//         } else {
-//             console.log(numerator/denumerator)
-//         }  
+     
+//     try {
+//             if (!(typeof numerator === 'number') || !(typeof denumerator === 'number')) {
+//                 throw new Error('Parameters are not a number')
+//            } else if (denumerator === 0) {
+//               throw new Error('Dividing to 0 is forbitten')
+//              } 
+//         return numerator/denumerator
+//       }
     
-// }
-// catch (error) {
-//     console.log(error)
-//   } 
-// finally {
-//     console.log('Dividing is finished')
-//        }
+//      catch (error) {
+//          console.log(error)
+//       } 
+//      finally {
+//          console.log('Dividing is finished')
+//             }
+// } 
 
 
 //console.log(divide('gh',7))
